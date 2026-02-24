9b2a6500a7c50b698c3e17fa7de0928a986f6d92
jcaps-ops <jaxon.castro@ucas-edu.net>
2026-02-24 00:51:27 +0000

    I have been freed from this eternal hell

--- Word_counter/source/file_reader.py
+++ Word_counter/source/file_reader.py
@@ -1,49 +1,45 @@
+
+#This function prints out all that we need to see Yippe by just looping over the document 
 def word_counter(Good_friday):
     try:
         with open(Good_friday, "r", newline = "") as document:
             doc = []
-            data = []
-            ext_data = False
-            for line in document:
-                if ext_data == False:
-                    if line != "-------":
-                        doc.append(line)
-                    else:
-                        ext_data = True
-                else:
-                    data.append[line]
-            for x in doc:
+            for x in document:
                 print(x)
-            for y in data:
-                print(y)
+                doc += x
+            print(doc)
             return(doc)
     except:
         print("That file does not work")
-def add(Good_friday,doc,time):
+#This functions add lines to the document by just appeneding them to the document and the time and word count
+#Future jaxon here after i coded this I DESPISE THIS SECTION JUST STRIKE ME DOWN PLEASE
+def add(Good_friday,doc,time,word_count):
     try:
         with open(Good_friday, "a", newline = "") as document:
-            document.truncate(0)
-            for x in doc:
-                document.write(x)
-            while True:
+            answering = True
+            while answering:
                 doc_true = input("What line would you like to add(to exit type):")
                 if doc_true == "exit":
-                    break
+                    answering = False
                 else:
                     document.write(doc_true)
-            document.write("")
-            document.write("-------")
-            words = []
-            for x in document:
-                words.append(x.split)
-            words_len = words.len()
-            document.write(f"The word count is {words_len}")
-            document.write(f"The last accesed data is {time[0]} the last accesed time was {time[1]}")
-
+                    document.write("\n")
                     
 
-
+            document.write(f"The time last date is {time[0]} The last acess time is {time[1]}\n")
+            document.write(f"The word count is {word_count}\n")
     except:
         print("That file does not work")
 
+#This one is just to get the word count we need
+def word_counts(Good_friday):
+    try:
+        count = 0
+        with open(Good_friday, "r", newline = "") as document:
+            data = document.read()
+            words = data.split()
+            count += len(words)
+            return(count)
+    except:
+        print("That file does not work")
 
--- Word_counter/source/main.py
+++ Word_counter/source/main.py
@@ -1,7 +1,8 @@
-from file_reader import word_counter,add
+from file_reader import word_counter,add,word_counts
 from time_tracker import get_time
 
 def main():
+    ply_int = "none"
     while True:
         print("--- Word counter ---")
         print("1.Set the document")
@@ -11,17 +12,19 @@ def main():
         ply_imp = input("please imput(1-4):")
         if ply_imp == "1":
             ply_int = input("What file do you want to use:")
-        elif ply_imp == "2":
+        if ply_imp == "2":
             doc = word_counter(ply_int)
         elif ply_imp == "3":
             doc = word_counter(ply_int)
             time = get_time()
-            add(ply_int,doc,time)
+            word_count = word_counts(ply_int)
+            add(ply_int,doc,time,word_count)
         elif ply_imp == "4":
             break
         else:
             print("that is not an option")
-        
+
+
 
 main()
     
