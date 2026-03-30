648393ce7362ac06aca05f4caa3b87a27256ee3f
jcaps-ops <jaxon.castro@ucas-edu.net>
2026-03-30 22:23:48 +0000

    main

--- Grade_book/SRC/Main.py
+++ Grade_book/SRC/Main.py
@@ -4,7 +4,7 @@ from Saving import *
 def main_menu(book1):
     answer = True
     while answer == True:
-        respince = input("Main menu 1 to add a student, 2 to view the students 3 to add a grade")
+        respince = input(f"Main menu\n1 to add a student\n2 to view the students\n3 to add a grade\n 4 to exit")
         if respince == "1":
             name = input("What is the students name")
             stuID = input("What is the students ID")
@@ -19,13 +19,19 @@ def main_menu(book1):
             grading = True
             while grading:
                 newgrade = input("What is the grade (0-100):")
-                if newgrade.isnumaric():
+                if newgrade.isnumeric():
                     newgrade = int(newgrade)
                     if newgrade > 0 and newgrade < 101:
                         grading = False
             tempindex = book1.students.index(tempstudent)
             book1.students[tempindex][2].append(newgrade)
+            Currentstudent = student(book1.students[tempindex][0], book1.students[tempindex][1], book1.students[tempindex][2])
+            book1.add_student(Currentstudent.name,Currentstudent.id,Currentstudent.grades,Currentstudent.avargeGrade,Currentstudent.letter)
+            book1.students.pop(tempindex)
             
+            print(book1.students[tempindex][2])
+        if respince == "4":
+            answer = False
         
 
 
